15330ae0e67f37e716ea33318b53c346bae83f66
Vladimir Kirov <vladimir.kirov@siteground.com>
2020-12-06 11:05:17 +0200

    add noecho function

--- draw.sh
+++ draw.sh
@@ -12,6 +12,14 @@ function repeat {
   done
 }
 
+function disable_echo {
+  stty -echo
+}
+
+function enable_echo {
+  stty echo
+}
+
 function draw::code {
   local code="$*"
   printf '\e[%s' "${code}"
@@ -57,8 +65,8 @@ function draw::move_left {
 
 function draw::nchars {
   local count=$1
-  local char=$2
-  printf "%${count}s" | sed "s@ @${char}@g"
+  local char=${2:0:1}
+  printf "%.0s${char}" "$(seq 1 $((count)))"
 }
 
 function draw::line {
@@ -94,5 +102,14 @@ function draw::vline {
 # draw_right   [x] [y] [character]
 # draw_bottom  [x] [y] [character]
 # draw_corners [w:h] [x] [y] [characters]
-draw::line
 
+#	The parameters taken are
+# 1. win: the window on which to operate
+# 2. ls: character to be used for the left side of the window
+# 3. rs: character to be used for the right side of the window
+# 4. ts: character to be used for the top side of the window
+# 5. bs: character to be used for the bottom side of the window
+# 6. tl: character to be used for the top left corner of the window
+# 7. tr: character to be used for the top right corner of the window
+# 8. bl: character to be used for the bottom left corner of the window
+# 9. br: character to be used for the bottom right corner of the window
