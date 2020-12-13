4eaadeba2edeed651e67851aeb300e310ecbe702
Vladimir Kirov <vladimir.kirov@siteground.com>
2020-12-13 15:13:19 +0200

    draft functions for worm.sh

--- worm.sh
+++ worm.sh
@@ -6,10 +6,19 @@
 # ╯ LIGHT ARC UP AND LEFT
 # ╰ LIGHT ARC UP AND RIGHT
 
-case $current in
-  ─) next=;;
-# │ LIGHT VERTICAL
-# ╭ LIGHT ARC DOWN AND RIGHT
-# ╮ LIGHT ARC DOWN AND LEFT
-# ╯ LIGHT ARC UP AND LEFT
-# ╰ LIGHT ARC UP AND RIGHT
+function init {
+  pos=( [x]=0 [y]=0 )
+  previous="─"
+}
+
+function next_character {
+  case ${current} in
+    ─) next=;;
+    │) next=;;
+    ╭) next=;;
+    ╮) next=;;
+    ╯) next=;;
+    ╰) next=;;
+    *) :;;
+  esac
+}
